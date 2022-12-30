d63aa96b30a3f29fa5db4b69b153f2c7e76951df
Jaisson Valbuena <jaissonvalbuena29@outlook.com>
2022-12-30 13:39:19 -0500

    Functional script

--- script/index.js
+++ script/index.js
@@ -9,30 +9,97 @@ const paragraph1 = document.getElementById('firstText')
 const paragraph2 = document.getElementById('secondText')
 // Image in the aside
 const doll = document.getElementById('doll')
+// Clipboard Button
+// const clipboard = document.getElementById('clipboardButton')
 
 // Auto focus the cursor into the textarea
 message.focus()
 
-
-encryptButton.onclick = function() {
+// Button onClick encrypt the message
+encryptButton.onclick = function () {
   // Condition to know the textarea is empty or not
-  if(message.value != ""){
-    let encrypted = encrypt(message)  
-    alert(encrypted.value)
-  }else{
+  if (message.value != '') {
+    let encrypted = encrypt(message.value)
+    doll.style.display = 'none'
+    paragraph1.textContent = encrypted
+    paragraph2.textContent = ''
+    // clipboard.style.display = 'block'
+    // alert(encrypted)
+  } else {
     alert('Debes de ingresar un texto para continuar')
   }
 }
 
-decryptButton.onclick = function() {
-  let decrypted = dencrypt(message)
+// Button onClick decrypt the message
+decryptButton.onclick = function () {
+  // Condition to know the textarea is empty or not
+  if (message.value != '') {
+    let dencrypted = decrypt(message.value)
+    doll.style.display = 'none'
+    paragraph1.textContent = dencrypted
+    paragraph2.textContent = ''
+    // clipboard.style.display = 'block'
+    // alert(dencrypted)
+  } else {
+    alert('Debes de ingresar un texto para continuar')
+  }
 }
 
-function encrypt(msg) {
+// Button onClick save the final message to the clipboard
+clipboard.onclick = function () {
+  let texto = paragraph1.textContent
+  texto.select()
+  navigator.clipboard.readText(message.value)
+  texto.value = ''
+}
 
+// Function to replace the characters to encrypt
+function encrypt(msg) {
+  // Replace 'e' to 'enter'
+  if (msg.includes('e')) {
+    msg = msg.replaceAll('e', 'enter')
+  }
+  // Replace 'i' to 'imes'
+  if (msg.includes('i')) {
+    msg = msg.replaceAll('i', 'imes')
+  }
+  // Replace 'a' to 'ai'
+  if (msg.includes('a')) {
+    msg = msg.replaceAll('a', 'ai')
+  }
+  // Replace 'o' to 'ober'
+  if (msg.includes('o')) {
+    msg = msg.replaceAll('o', 'ober')
+  }
+  // Replace 'u' to 'ufat'
+  if (msg.includes('u')) {
+    msg = msg.replaceAll('u', 'ufat')
+  }
   return msg
 }
 
+// Function to replace the encrypt letter to the original character
 function decrypt(msg) {
+  msg = msg.toLowerCase()
+  // Replace 'enter' to 'e'
+  if (msg.includes('enter')) {
+    msg = msg.replaceAll('enter', 'e')
+  }
+  // Replace 'imes' to 'i'
+  if (msg.includes('imes')) {
+    msg = msg.replaceAll('imes', 'i')
+  }
+  // Replace 'ai' to 'a'
+  if (msg.includes('ai')) {
+    msg = msg.replaceAll('ai', 'a')
+  }
+  // Replace 'ober' to 'o'
+  if (msg.includes('ober')) {
+    msg = msg.replaceAll('ober', 'o')
+  }
+  // Replace 'ufat' to 'u'
+  if (msg.includes('ufat')) {
+    msg = msg.replaceAll('ufat', 'u')
+  }
   return msg
 }
